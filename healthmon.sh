442331543d183fb3bd31818f7ca472d23132f7f1
BenjaminUA92 <127944691+BenjaminUA92@users.noreply.github.com>
2024-06-26 10:29:48 +0100

    Update healthmon.sh

--- healthmon.sh
+++ healthmon.sh
@@ -1,3 +1,3 @@
 echo -e "Disk space:    \n$(df)\n\n" >> /var/log/healthmon.log
 echo -e "Memory usage:  \n$(free -m)\n\n" >> /var/log/healthmon.log
-echo -e "CPU usage:     \n$(top)\n\n" >> /var/log/healthmon.log
+echo -e "CPU usage:     \n$(top -bn1) \n\n" >> /var/log/healthmon.log
